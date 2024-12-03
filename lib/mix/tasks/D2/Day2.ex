306f12d2b62691d76199661c236188101801e856
Eoic <karolis.strazdas@protonmail.com>
2024-12-03 20:03:02 +0200

    Refactor day 2.

--- lib/mix/tasks/D2/Day2.ex
+++ lib/mix/tasks/D2/Day2.ex
@@ -19,28 +19,28 @@ defmodule Mix.Tasks.Day2 do
     |> Enum.reverse()
   end
 
-  defp in_range(deltas), do: not Enum.any?(deltas, &(abs(&1) not in 1..3))
-
-  defp all_positive(deltas), do: Enum.all?(deltas, &(&1 > 0))
-
-  defp all_negative(deltas), do: Enum.all?(deltas, &(&1 < 0))
-
   defp dampen(report, index, fun), do: fun.(List.delete_at(report, index))
 
-  defp is_stable?(report) do
+  defp in_range?(deltas), do: not Enum.any?(deltas, &(abs(&1) not in 1..3))
+
+  defp all_positive?(deltas), do: Enum.all?(deltas, &(&1 > 0))
+
+  defp all_negative?(deltas), do: Enum.all?(deltas, &(&1 < 0))
+
+  defp stable?(report) do
     report
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.map(fn [left, right] -> right - left end)
-    |> (&(in_range(&1) and (all_positive(&1) or all_negative(&1)))).()
+    |> (&(in_range?(&1) and (all_positive?(&1) or all_negative?(&1)))).()
   end
 
-  defp part_one(data), do: Enum.count(data, &is_stable?/1)
+  defp part_one(data), do: Enum.count(data, &stable?/1)
 
   defp part_two(data) do
     data
     |> Enum.count(fn report ->
       0..(Enum.count(report) - 1)
-      |> Enum.any?(fn index -> dampen(report, index, &is_stable?/1) end)
+      |> Enum.any?(fn index -> dampen(report, index, &stable?/1) end)
     end)
   end
 
