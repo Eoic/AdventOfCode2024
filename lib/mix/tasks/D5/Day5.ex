d5e199d65b9b0d238a60748a96b5e693361bb83c
Eoic <karolis.strazdas@protonmail.com>
2024-12-05 23:11:05 +0200

    Refactor day 5.

--- lib/mix/tasks/D5/Day5.ex
+++ lib/mix/tasks/D5/Day5.ex
@@ -8,15 +8,14 @@ defmodule Mix.Tasks.Day5 do
     [rule_lines, update_lines] =
       __ENV__.file
       |> InputUtils.read_all(@input)
-      |> String.trim()
-      |> String.split(~r/\r\n\r\n/)
+      |> String.split(~r/\n\n/)
 
     [parse_rules(rule_lines), parse_updates(update_lines)]
   end
 
   defp parse_rules(rule_lines) do
     rule_lines
-    |> String.split(~r/\r\n/)
+    |> String.split(~r/\n/)
     |> Enum.reduce(%{}, fn line, acc ->
       [left, right] =
         line
@@ -29,45 +28,30 @@ defmodule Mix.Tasks.Day5 do
 
   defp parse_updates(update_lines) do
     update_lines
-    |> String.split(~r/\r\n/, trim: true)
+    |> String.split(~r/\n/, trim: true)
     |> Enum.map(fn line ->
       line
-      |> String.split(",", trim: true)
+      |> String.split(~r/\,/)
       |> Enum.map(&String.to_integer/1)
     end)
   end
 
-  defp find_valid_updates([rules, updates]) do
-    Enum.filter(updates, fn line -> line === Enum.sort(line, &(&2 in Map.get(rules, &1, []))) end)
-  end
-
-  defp find_invalid_updates([rules, updates]) do
-    Enum.flat_map(updates, fn line ->
+  defp find_split_updates([rules, updates]) do
+    Enum.reduce(updates, %{valid: [], invalid: []}, fn line, groups ->
       sorted_line = Enum.sort(line, &(&2 in Map.get(rules, &1, [])))
-      if sorted_line !== line, do: [sorted_line], else: []
+
+      if line === sorted_line,
+        do: %{groups | valid: [line | Map.get(groups, :valid)]},
+        else: %{groups | invalid: [sorted_line | Map.get(groups, :invalid)]}
     end)
   end
 
-  defp sum_medians(updates) do
-    Enum.reduce(updates, 0, &(&2 + Enum.at(&1, div(length(&1), 2))))
-  end
-
-  defp part_one(data) do
-    data
-    |> find_valid_updates()
-    |> sum_medians()
-  end
-
-  defp part_two(data) do
-    data
-    |> find_invalid_updates()
-    |> sum_medians()
-  end
+  defp sum_medians(updates), do: Enum.reduce(updates, 0, &(&2 + Enum.at(&1, div(length(&1), 2))))
 
   def run(_) do
-    data = parse_input()
-    p1_result = Timer.measure(fn -> part_one(data) end, "Part 1")
-    p2_result = Timer.measure(fn -> part_two(data) end, "Part 2")
+    %{:valid => valid, :invalid => invalid} = find_split_updates(parse_input())
+    p1_result = Timer.measure(fn -> sum_medians(valid) end, "Part 1")
+    p2_result = Timer.measure(fn -> sum_medians(invalid) end, "Part 2")
     IO.puts("| Part one: #{p1_result} |")
     IO.puts("| Part two: #{p2_result} |")
   end
