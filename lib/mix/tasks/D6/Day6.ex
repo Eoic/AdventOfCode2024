410f5f1e339acd7e770360d15e2663f17746579e
Eoic <karolis.strazdas@protonmail.com>
2024-12-06 23:09:54 +0200

    Refactor day 6.

--- lib/mix/tasks/D6/Day6.ex
+++ lib/mix/tasks/D6/Day6.ex
@@ -3,6 +3,7 @@ defmodule Mix.Tasks.Day6 do
   require InputUtils
 
   @input "input.txt"
+  @default_direction [0, -1]
   @directions %{
     [-1, 0] => :left,
     [1, 0] => :right,
@@ -14,35 +15,34 @@ defmodule Mix.Tasks.Day6 do
     __ENV__.file
     |> InputUtils.read_lines(@input)
     |> Enum.reduce(%{obstacles: MapSet.new(), guard: nil, height: 0}, fn line, grid ->
-      tiles = String.graphemes(line)
-
-      tiles
+      line
+      |> String.graphemes()
       |> Enum.with_index()
-      |> Enum.reduce(grid, fn {token, x}, grid = %{:obstacles => obstacles, :height => y} ->
+      |> Enum.reduce(grid, fn {token, x}, grid = %{:height => y} ->
         case token do
-          "#" -> %{grid | :obstacles => MapSet.put(obstacles, [x, y])}
+          "#" -> %{grid | :obstacles => MapSet.put(grid.obstacles, [x, y])}
           "^" -> %{grid | :guard => [x, y]}
           _ -> grid
         end
       end)
       |> Map.update(:height, 1, &(&1 + 1))
-      |> Map.update(:width, length(tiles), & &1)
+      |> Map.update(:width, String.length(line), & &1)
     end)
   end
 
-  def is_inside_map?([x, y], width, height) do
+  defp is_inside?([x, y], width, height) do
     x >= 0 and y >= 0 and x < width and y < height
   end
 
-  def get_tile_action([px, py], obstacles, width, height) do
+  defp get_tile_action([px, py], %{:obstacles => obstacles, :width => width, :height => height}) do
     cond do
       MapSet.member?(obstacles, [px, py]) -> :turn
-      is_inside_map?([px, py], width, height) -> :walk
+      is_inside?([px, py], width, height) -> :walk
       true -> :stop
     end
   end
 
-  def turn_right(direction) do
+  defp turn_right(direction) do
     case direction do
       [0, -1] -> [1, 0]
       [1, 0] -> [0, 1]
@@ -51,14 +51,14 @@ defmodule Mix.Tasks.Day6 do
     end
   end
 
-  def apply_direction([px, py], [dx, dy]), do: [px + dx, py + dy]
+  defp apply_direction([px, py], [dx, dy]), do: [px + dx, py + dy]
 
-  def update_extra_obstacles(
-        position = [px, py],
-        direction,
-        obstacles,
-        extra_obstacles
-      ) do
+  defp update_extra_obstacles(
+         position = [px, py],
+         direction,
+         obstacles,
+         extra_obstacles
+       ) do
     right_side_obstacle =
       case Map.get(@directions, direction, nil) do
         :left ->
@@ -79,136 +79,83 @@ defmodule Mix.Tasks.Day6 do
       else: extra_obstacles
   end
 
-  def solve_route(
-        obstacles,
-        width,
-        height,
-        position,
-        direction,
-        route,
-        extra_obstacles
-      ) do
+  defp trace_route(
+         grid,
+         position,
+         direction,
+         route \\ MapSet.new(),
+         extra_obstacles \\ MapSet.new()
+       )
+
+  defp trace_route(
+         grid,
+         position,
+         direction,
+         route,
+         extra_obstacles
+       ) do
     route = MapSet.put(route, position)
     next_position = apply_direction(position, direction)
-    action = get_tile_action(next_position, obstacles, width, height)
+    action = get_tile_action(next_position, grid)
 
     case action do
       :turn ->
-        solve_route(
-          obstacles,
-          width,
-          height,
-          position,
-          turn_right(direction),
-          route,
-          extra_obstacles
-        )
+        trace_route(grid, position, turn_right(direction), route, extra_obstacles)
 
       :walk ->
-        extra_obstacles = update_extra_obstacles(position, direction, obstacles, extra_obstacles)
+        extra_obstacles =
+          update_extra_obstacles(position, direction, grid.obstacles, extra_obstacles)
 
-        solve_route(
-          obstacles,
-          width,
-          height,
-          next_position,
-          direction,
-          route,
-          extra_obstacles
-        )
+        trace_route(grid, next_position, direction, route, extra_obstacles)
 
       :stop ->
         [route, extra_obstacles]
     end
   end
 
-  def is_looping?(
-        obstacles,
-        width,
-        height,
-        position,
-        direction,
-        visited,
-        is_first_time
-      ) do
-    if not is_first_time and MapSet.member?(visited, [position, direction]) do
-      true
-    else
-      next_position = apply_direction(position, direction)
-      action = get_tile_action(next_position, obstacles, width, height)
+  defp is_looping?(grid, position, direction, visited \\ MapSet.new())
 
-      case action do
+  defp is_looping?(grid, position, direction, visited) do
+    if not MapSet.member?(visited, [position, direction]) do
+      next_position = apply_direction(position, direction)
+
+      case get_tile_action(next_position, grid) do
         :turn ->
           visited = MapSet.put(visited, [position, direction])
-
-          is_looping?(
-            obstacles,
-            width,
-            height,
-            position,
-            turn_right(direction),
-            visited,
-            false
-          )
+          is_looping?(grid, position, turn_right(direction), visited)
 
         :walk ->
-          is_looping?(
-            obstacles,
-            width,
-            height,
-            next_position,
-            direction,
-            visited,
-            false
-          )
+          is_looping?(grid, next_position, direction, visited)
 
         :stop ->
           false
       end
+    else
+      true
     end
   end
 
-  def traverse(%{
-        :obstacles => obstacles,
-        :width => width,
-        :height => height,
-        :guard => guard
-      }) do
-    solve_route(obstacles, width, height, guard, [0, -1], MapSet.new(), MapSet.new())
-  end
+  defp count_loops(grid, extra_obstacles) do
+    [px, py] = grid.guard
 
-  defp count_loops(
-         %{
-           :obstacles => obstacles,
-           :width => width,
-           :height => height,
-           :guard => guard
-         },
-         extra_obstacles
-       ) do
-    [px, py] = guard
-    extra_obstacles = Map.delete(extra_obstacles, [[px, py - 1], [0, -1], [px, py]])
-
-    Enum.filter(extra_obstacles, fn obstacle ->
-      obstacles = MapSet.put(obstacles, obstacle)
-      is_looping?(obstacles, width, height, guard, [0, -1], MapSet.new(), true)
+    extra_obstacles
+    |> Map.delete([[px, py - 1], @default_direction, [px, py]])
+    |> Stream.filter(fn obstacle ->
+      grid = %{grid | :obstacles => MapSet.put(grid.obstacles, obstacle)}
+      is_looping?(grid, grid.guard, @default_direction)
     end)
     |> Enum.count()
   end
 
-  defp part_one(route) do
-    MapSet.size(route)
-  end
+  defp part_one(route), do: MapSet.size(route)
 
-  defp part_two(map, extra_obstacles) do
-    count_loops(map, extra_obstacles)
-  end
+  defp part_two(grid, extra_obstacles), do: count_loops(grid, extra_obstacles)
 
   def run(_) do
-    map = parse_input()
-    [route, extra_obstacles] = traverse(map)
+    grid = parse_input()
+    [route, extra_obstacles] = trace_route(grid, grid.guard, @default_direction)
     p1_result = Timer.measure(fn -> part_one(route) end, "Part 1")
-    p2_result = Timer.measure(fn -> part_two(map, extra_obstacles) end, "Part 2")
+    p2_result = Timer.measure(fn -> part_two(grid, extra_obstacles) end, "Part 2")
     IO.puts("| Part one: #{p1_result} |")
     IO.puts("| Part two: #{p2_result} |")
   end
