9aecc6717481d09374f6a60e022958e74f271288
Eoic <karolis.strazdas@protonmail.com>
2024-12-11 22:52:21 +0200

    Solve day 11.

--- lib/mix/tasks/D10/Day10.ex
+++ lib/mix/tasks/D10/Day10.ex
@@ -1,141 +1,141 @@
-defmodule Mix.Tasks.Day10 do
-  require Timer
-  require InputUtils
+# defmodule Mix.Tasks.Day10 do
+#   require Timer
+#   require InputUtils
 
-  @input "sample2.txt"
+#   @input "sample2.txt"
 
-  defp parse_input() do
-    __ENV__.file
-    |> InputUtils.read_all(@input)
-    |> String.split(~r/\n/)
-    |> Enum.reduce(%{width: 0, height: 0, grid: %{}, starts: []}, fn row, map = %{:height => y} ->
-      row
-      |> String.graphemes()
-      |> Enum.with_index()
-      |> Enum.reduce(map, fn {cell, x}, map ->
-        cell = String.to_integer(cell)
+#   defp parse_input() do
+#     __ENV__.file
+#     |> InputUtils.read_all(@input)
+#     |> String.split(~r/\n/)
+#     |> Enum.reduce(%{width: 0, height: 0, grid: %{}, starts: []}, fn row, map = %{:height => y} ->
+#       row
+#       |> String.graphemes()
+#       |> Enum.with_index()
+#       |> Enum.reduce(map, fn {cell, x}, map ->
+#         cell = String.to_integer(cell)
 
-        map =
-          if cell == 0 do
-            %{map | starts: [{[x, y], 0} | map.starts]}
-          else
-            %{map | grid: Map.put(map.grid, [x, y], cell)}
-          end
+#         map =
+#           if cell == 0 do
+#             %{map | starts: [{[x, y], 0} | map.starts]}
+#           else
+#             %{map | grid: Map.put(map.grid, [x, y], cell)}
+#           end
 
-        %{map | width: x + 1}
-      end)
-      |> Map.update(:height, y + 1, &(&1 + 1))
-    end)
-    |> Kernel.then(fn map -> %{map | width: map.width + 1} end)
-  end
+#         %{map | width: x + 1}
+#       end)
+#       |> Map.update(:height, y + 1, &(&1 + 1))
+#     end)
+#     |> Kernel.then(fn map -> %{map | width: map.width + 1} end)
+#   end
 
-  def is_neighbor?({[cx, cy], current_cell}, {[nx, ny], next_cell}, visited) do
-    next_cell - current_cell === 1 and
-      abs(cx - nx) + abs(cy - ny) === 1 and
-      not MapSet.member?(visited, [nx, ny])
-  end
+#   def is_neighbor?({[cx, cy], current_cell}, {[nx, ny], next_cell}, visited) do
+#     next_cell - current_cell === 1 and
+#       abs(cx - nx) + abs(cy - ny) === 1 and
+#       not MapSet.member?(visited, [nx, ny])
+#   end
 
-  # def get_lowest_unvisited(positions, visited) do
-  #   Enum.find(positions, fn position -> not MapSet.member?(visited, position) end)
-  # end
+#   # def get_lowest_unvisited(positions, visited) do
+#   #   Enum.find(positions, fn position -> not MapSet.member?(visited, position) end)
+#   # end
 
-  def traverse([], _, _visited, path), do: Enum.reverse(path)
+#   def traverse([], _, _visited, path), do: Enum.reverse(path)
 
-  def traverse([current = {[cx, cy], cell} | tail], positions, visited, path) do
-    if cell === 9 do
-      IO.inspect(["Visited 9", [cx, cy]])
-    end
+#   def traverse([current = {[cx, cy], cell} | tail], positions, visited, path) do
+#     if cell === 9 do
+#       IO.inspect(["Visited 9", [cx, cy]])
+#     end
 
-    if MapSet.member?(visited, [cx, cy]) do
-      traverse(tail, positions, visited, path)
-    else
-      visited = MapSet.put(visited, [cx, cy])
+#     if MapSet.member?(visited, [cx, cy]) do
+#       traverse(tail, positions, visited, path)
+#     else
+#       visited = MapSet.put(visited, [cx, cy])
 
-      neighbors =
-        positions
-        |> Enum.filter(fn next -> is_neighbor?(current, next, visited) end)
-        |> Enum.sort_by(fn {[_x, _y], cell} -> cell end)
+#       neighbors =
+#         positions
+#         |> Enum.filter(fn next -> is_neighbor?(current, next, visited) end)
+#         |> Enum.sort_by(fn {[_x, _y], cell} -> cell end)
 
-      if length(neighbors) === 0 and cell !== 9 do
-        IO.inspect(["Dead end at", {[cx, cy], cell}], charlists: :as_lists)
-      end
+#       if length(neighbors) === 0 and cell !== 9 do
+#         IO.inspect(["Dead end at", {[cx, cy], cell}], charlists: :as_lists)
+#       end
 
-      traverse(tail ++ neighbors, positions, visited, [current | path])
-    end
-  end
+#       traverse(tail ++ neighbors, positions, visited, [current | path])
+#     end
+#   end
 
-  # def trace_path(current, positions, visited, path) do
-  #   visited = MapSet.put(visited, current)
-  #   next = Enum.find(positions, fn next -> is_neighbor?(current, next, MapSet.new()) end)
+#   # def trace_path(current, positions, visited, path) do
+#   #   visited = MapSet.put(visited, current)
+#   #   next = Enum.find(positions, fn next -> is_neighbor?(current, next, MapSet.new()) end)
 
-  #   if next === nil do
-  #     {[current | path], visited}
-  #   else
-  #     trace_path(next, positions, visited, [current | path])
-  #   end
-  # end
+#   #   if next === nil do
+#   #     {[current | path], visited}
+#   #   else
+#   #     trace_path(next, positions, visited, [current | path])
+#   #   end
+#   # end
 
-  # def count_alternative_paths(path, visited, alt_path_count) do
-  #   unvisited = get_lowest_unvisited(path, visited)
+#   # def count_alternative_paths(path, visited, alt_path_count) do
+#   #   unvisited = get_lowest_unvisited(path, visited)
 
-  #   if unvisited === nil do
-  #     alt_path_count
-  #   else
-  #     {alt_path, visited} = trace_path(unvisited, path, visited, [])
-  #     IO.inspect(alt_path)
-  #     count_alternative_paths(path, visited, alt_path_count + 1)
-  #   end
-  # end
+#   #   if unvisited === nil do
+#   #     alt_path_count
+#   #   else
+#   #     {alt_path, visited} = trace_path(unvisited, path, visited, [])
+#   #     IO.inspect(alt_path)
+#   #     count_alternative_paths(path, visited, alt_path_count + 1)
+#   #   end
+#   # end
 
-  def count_junctions([], _, counts), do: counts
+#   def count_junctions([], _, counts), do: counts
 
-  def count_junctions([current = {[x, y], cell} | path], positions, counts) do
-    neighbors = Enum.filter(positions, fn next -> is_neighbor?(current, next, MapSet.new()) end)
+#   def count_junctions([current = {[x, y], cell} | path], positions, counts) do
+#     neighbors = Enum.filter(positions, fn next -> is_neighbor?(current, next, MapSet.new()) end)
 
-    counts =
-      Map.update(counts, [x, y], length(neighbors), fn current_count ->
-        length(neighbors)
-      end)
+#     counts =
+#       Map.update(counts, [x, y], length(neighbors), fn current_count ->
+#         length(neighbors)
+#       end)
 
-    count_junctions(path, positions, counts)
-  end
+#     count_junctions(path, positions, counts)
+#   end
 
-  defp part_one(data) do
-    data.starts
-    |> Enum.map(fn start ->
-      traverse([start], Map.to_list(data.grid), MapSet.new(), [])
-      |> Enum.filter(fn {_, cell} -> cell === 9 end)
-      |> Enum.count()
-    end)
-    |> Enum.sum()
-  end
+#   defp part_one(data) do
+#     data.starts
+#     |> Enum.map(fn start ->
+#       traverse([start], Map.to_list(data.grid), MapSet.new(), [])
+#       |> Enum.filter(fn {_, cell} -> cell === 9 end)
+#       |> Enum.count()
+#     end)
+#     |> Enum.sum()
+#   end
 
-  defp part_two(data) do
-    data.starts
-    |> Enum.map(fn start ->
-      [start]
-      |> traverse(Map.to_list(data.grid), MapSet.new(), [])
-      # |> Kernel.then(fn path -> count_junctions(path, path, %{}) end)
-      # |> Map.to_list()
-      # |> Enum.map(fn {[x, y], count} -> count end)
-      # |> Enum.sum()
-      # |> Kernel.then(fn sum -> sum - 9 end)
-      # |> Enum.frequencies_by(fn {[_, _], cell} -> cell end)
-      |> IO.inspect()
+#   defp part_two(data) do
+#     data.starts
+#     |> Enum.map(fn start ->
+#       [start]
+#       |> traverse(Map.to_list(data.grid), MapSet.new(), [])
+#       # |> Kernel.then(fn path -> count_junctions(path, path, %{}) end)
+#       # |> Map.to_list()
+#       # |> Enum.map(fn {[x, y], count} -> count end)
+#       # |> Enum.sum()
+#       # |> Kernel.then(fn sum -> sum - 9 end)
+#       # |> Enum.frequencies_by(fn {[_, _], cell} -> cell end)
+#       |> IO.inspect()
 
-      # |> count_alternative_paths(MapSet.new(), 0)
+#       # |> count_alternative_paths(MapSet.new(), 0)
 
-      # |> IO.inspect()
-    end)
+#       # |> IO.inspect()
+#     end)
 
-    # |> Enum.sum()
-  end
+#     # |> Enum.sum()
+#   end
 
-  def run(_) do
-    data = Timer.measure(fn -> parse_input() end, "Input")
-    p1_result = Timer.measure(fn -> part_one(data) end, "Part 1")
-    p2_result = Timer.measure(fn -> part_two(data) end, "Part 2")
-    # IO.puts("| Part one: #{p1_result} |")
-    # IO.puts("| Part two: #{p2_result} |")
-  end
-end
+#   def run(_) do
+#     data = Timer.measure(fn -> parse_input() end, "Input")
+#     p1_result = Timer.measure(fn -> part_one(data) end, "Part 1")
+#     p2_result = Timer.measure(fn -> part_two(data) end, "Part 2")
+#     # IO.puts("| Part one: #{p1_result} |")
+#     # IO.puts("| Part two: #{p2_result} |")
+#   end
+# end
---
+++ lib/mix/tasks/D11/Day11.ex
@@ -0,0 +1,81 @@
+defmodule Mix.Tasks.Day11 do
+  require Timer
+  require InputUtils
+
+  @input "input.txt"
+
+  defp parse_input() do
+    __ENV__.file
+    |> InputUtils.read_all(@input)
+    |> String.split(~r/\s/, trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(%{}, fn stone, stones ->
+      Map.update(stones, stone, 1, &(&1 + 1))
+    end)
+  end
+
+  defp even_digits?(value) do
+    value
+    |> Integer.to_string()
+    |> String.length()
+    |> rem(2)
+    |> Kernel.===(0)
+  end
+
+  def apply_rule(value, stones, change) do
+    cond do
+      value === 0 ->
+        stones
+        |> Map.update(0, -change, &(&1 - change))
+        |> Map.update(1, change, &(&1 + change))
+
+      even_digits?(value) ->
+        value_str = Integer.to_string(value)
+        value_len = String.length(value_str)
+        {left, right} = String.split_at(value_str, div(value_len, 2))
+
+        stones
+        |> Map.update(String.to_integer(left), change, fn current -> current + change end)
+        |> Map.update(String.to_integer(right), change, fn current -> current + change end)
+        |> Map.update(value, -change, fn current -> current - change end)
+
+      true ->
+        stones
+        |> Map.update(value, -change, fn current -> current - change end)
+        |> Map.update(value * 2024, change, fn current -> current + change end)
+    end
+  end
+
+  def apply_all(stones) do
+    stones
+    |> Map.to_list()
+    |> Enum.reduce(stones, fn {value, count}, stones ->
+      apply_rule(value, stones, count)
+    end)
+  end
+
+  def blink(stones, count) do
+    0..(count - 1)//1
+    |> Enum.reduce(stones, fn _, stones -> apply_all(stones) end)
+  end
+
+  defp count_stones(stones, blinks) do
+    stones
+    |> blink(blinks)
+    |> Map.filter(fn {_, count} -> count > 0 end)
+    |> Map.to_list()
+    |> Enum.reduce(0, fn {_, count}, sum -> sum + count end)
+  end
+
+  defp part_one(stones), do: count_stones(stones, 25)
+
+  defp part_two(stones), do: count_stones(stones, 75)
+
+  def run(_) do
+    data = Timer.measure(fn -> parse_input() end, "Input")
+    p1_result = Timer.measure(fn -> part_one(data) end, "Part 1")
+    p2_result = Timer.measure(fn -> part_two(data) end, "Part 2")
+    IO.puts("| Part one: #{p1_result} |")
+    IO.puts("| Part two: #{p2_result} |")
+  end
+end
