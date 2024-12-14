d77a792ac2c388f0628d4b2b200d353fbf2dc86d
Eoic <karolis.strazdas@protonmail.com>
2024-12-14 13:30:52 +0200

    Update.

--- lib/mix/tasks/D12/Day12.ex
+++ lib/mix/tasks/D12/Day12.ex
@@ -1,169 +1,174 @@
-defmodule Mix.Tasks.Day12 do
-  require Timer
-  require InputUtils
+# TODO: Redfactor.
+# ---
 
-  @input "sample1.txt"
+# defmodule Mix.Tasks.Day12 do
+#   require Timer
+#   require InputUtils
 
-  defp parse_input() do
-    __ENV__.file
-    |> InputUtils.read_all(@input)
-    |> String.split(~r/\n/, trim: true)
-    |> Enum.reduce(%{width: 0, height: 0, grid: %{}, labels: Map.new()}, fn row, map ->
-      row
-      |> String.graphemes()
-      |> Enum.with_index()
-      |> Enum.reduce(map, fn {cell, x}, map ->
-        y = Map.get(map, :height, 0)
-        labels = Map.update(map.labels, cell, 1, fn count -> count + 1 end)
-        %{map | width: x + 1, grid: Map.put(map.grid, [x, y], cell), labels: labels}
-      end)
-      |> Map.update(:height, 1, &(&1 + 1))
-    end)
-  end
+#   @input "sample0.txt"
 
-  def find_neighbors([cx, cy], positions) do
-    Enum.filter(positions, fn [nx, ny] ->
-      abs(nx - cx) + abs(ny - cy) === 1
-    end)
-  end
+#   defp parse_input() do
+#     __ENV__.file
+#     |> InputUtils.read_all(@input)
+#     |> String.split(~r/\n/, trim: true)
+#     |> Enum.reduce(%{width: 0, height: 0, grid: %{}, labels: Map.new()}, fn row, map ->
+#       row
+#       |> String.graphemes()
+#       |> Enum.with_index()
+#       |> Enum.reduce(map, fn {cell, x}, map ->
+#         y = Map.get(map, :height, 0)
+#         labels = Map.update(map.labels, cell, 1, fn count -> count + 1 end)
+#         %{map | width: x + 1, grid: Map.put(map.grid, [x, y], cell), labels: labels}
+#       end)
+#       |> Map.update(:height, 1, &(&1 + 1))
+#     end)
+#   end
 
-  def map_edges_touched([cx, cy], size) do
-    [
-      cx - 1 < 0,
-      cx + 1 >= size.width,
-      cy - 1 < 0,
-      cy + 1 >= size.height
-    ]
-    |> Enum.count(fn condition -> condition end)
-  end
+#   def find_neighbors([cx, cy], positions) do
+#     Enum.filter(positions, fn [nx, ny] ->
+#       abs(nx - cx) + abs(ny - cy) === 1
+#     end)
+#   end
 
-  def find_perimeter([], _positions, _size, _visited, perimeter, border_points),
-    do: [perimeter, border_points]
+#   def map_edges_touched([cx, cy], size) do
+#     [
+#       cx - 1 < 0,
+#       cx + 1 >= size.width,
+#       cy - 1 < 0,
+#       cy + 1 >= size.height
+#     ]
+#     |> Enum.count(fn condition -> condition end)
+#   end
 
-  def find_perimeter([current | queue], positions, size, visited, perimeter, border_points) do
-    if MapSet.member?(visited, current) do
-      find_perimeter(queue, positions, size, visited, perimeter, border_points)
-    else
-      visited = MapSet.put(visited, current)
-      all_neighbors = find_neighbors(current, positions)
+#   def find_perimeter([], _positions, _size, _visited, perimeter, border_points),
+#     do: [perimeter, border_points]
 
-      unvisited_neighbors =
-        Enum.filter(all_neighbors, fn neighbor -> not MapSet.member?(visited, neighbor) end)
+#   def find_perimeter([current | queue], positions, size, visited, perimeter, border_points) do
+#     if MapSet.member?(visited, current) do
+#       find_perimeter(queue, positions, size, visited, perimeter, border_points)
+#     else
+#       visited = MapSet.put(visited, current)
+#       all_neighbors = find_neighbors(current, positions)
 
-      neighbors_count = length(all_neighbors)
-      perimeter = perimeter + (4 - neighbors_count)
-      border_points = if neighbors_count < 4, do: [current | border_points], else: border_points
+#       unvisited_neighbors =
+#         Enum.filter(all_neighbors, fn neighbor -> not MapSet.member?(visited, neighbor) end)
 
-      find_perimeter(
-        queue ++ unvisited_neighbors,
-        positions,
-        size,
-        visited,
-        perimeter,
-        border_points
-      )
-    end
-  end
+#       neighbors_count = length(all_neighbors)
+#       perimeter = perimeter + (4 - neighbors_count)
+#       border_points = if neighbors_count < 4, do: [current | border_points], else: border_points
 
-  def is_neighbor?([cx, cy], [nx, ny], visited) do
-    abs(nx - cx) + abs(ny - cy) === 1 and not MapSet.member?(visited, [nx, ny])
-  end
+#       find_perimeter(
+#         queue ++ unvisited_neighbors,
+#         positions,
+#         size,
+#         visited,
+#         perimeter,
+#         border_points
+#       )
+#     end
+#   end
 
-  def find_cluster([], _, _, cluster), do: cluster
+#   def is_neighbor?([cx, cy], [nx, ny], visited) do
+#     abs(nx - cx) + abs(ny - cy) === 1 and not MapSet.member?(visited, [nx, ny])
+#   end
 
-  def find_cluster([current | queue], positions, visited, cluster) do
-    if MapSet.member?(visited, current) do
-      find_cluster(queue, positions, visited, cluster)
-    else
-      visited = MapSet.put(visited, current)
-      neighbors = Enum.filter(positions, fn next -> is_neighbor?(current, next, visited) end)
-      find_cluster(queue ++ neighbors, positions, visited, [current | cluster])
-    end
-  end
+#   def find_cluster([], _, _, cluster), do: cluster
 
-  def get_all_clusters([], _, clusters), do: clusters
+#   def find_cluster([current | queue], positions, visited, cluster) do
+#     if MapSet.member?(visited, current) do
+#       find_cluster(queue, positions, visited, cluster)
+#     else
+#       visited = MapSet.put(visited, current)
+#       neighbors = Enum.filter(positions, fn next -> is_neighbor?(current, next, visited) end)
+#       find_cluster(queue ++ neighbors, positions, visited, [current | cluster])
+#     end
+#   end
 
-  def get_all_clusters(positions, size, clusters) do
-    cluster = find_cluster([hd(positions)], positions, MapSet.new(), [])
+#   def get_all_clusters([], _, clusters), do: clusters
 
-    if cluster === [] do
-      clusters
-    else
-      new_positions =
-        Enum.filter(positions, fn position -> not Enum.member?(cluster, position) end)
+#   def get_all_clusters(positions, size, clusters) do
+#     cluster = find_cluster([hd(positions)], positions, MapSet.new(), [])
 
-      get_all_clusters(new_positions, size, [cluster | clusters])
-    end
-  end
+#     if cluster === [] do
+#       clusters
+#     else
+#       new_positions =
+#         Enum.filter(positions, fn position -> not Enum.member?(cluster, position) end)
 
-  def reduce_edges(edges) do
-  end
+#       get_all_clusters(new_positions, size, [cluster | clusters])
+#     end
+#   end
 
-  def create_edges(positions) do
-    IO.inspect(positions, label: "Provided positions")
+#   def create_edges(positions) do
+#     positions
+#     |> Enum.sort_by(fn [x, _y] -> x end, :asc)
+#     |> Enum.sort_by(fn [_x, y] -> y end, :asc)
+#     |> Enum.flat_map(fn [x, y] ->
+#       [
+#         {[x, y], [x + 1, y]},
+#         {[x, y], [x, y + 1]},
+#         {[x, y + 1], [x + 1, y + 1]},
+#         {[x + 1, y], [x + 1, y + 1]}
+#       ]
+#     end)
+#     |> Kernel.then(fn edges ->
+#       freq = Enum.frequencies(edges)
+#       Enum.reject(edges, fn edge -> Map.get(freq, edge) > 1 end)
+#     end)
+#     |> merge_edges()
+#   end
 
-    positions
-    |> Enum.sort_by(fn [x, _y] -> x end, :asc)
-    |> Enum.sort_by(fn [_x, y] -> y end, :asc)
-    |> Enum.flat_map(fn [x, y] ->
-      [
-        {[x, y], [x + 1, y]},
-        {[x, y], [x, y + 1]},
-        {[x, y + 1], [x + 1, y + 1]},
-        {[x + 1, y], [x + 1, y + 1]}
-      ]
-    end)
-    |> Kernel.then(fn edges ->
-      # IO.inspect(length(edges), label: "OK")
-      # TODO: Remove all points with frequency >= 2.
-      # Flatten entire points list and sort by [x, y].
-      # Take unique points.
-      # Trace path from sta
-      IO.inspect(Enum.frequencies(edges))
-      edges
-    end)
-  end
+#   def merge_edges(edges, merged \\ [])
 
-  defp part_one(map) do
-    map.labels
-    # |> Map.filter(fn {label, _count} -> label === "A" end)
-    |> Enum.reduce(0, fn {label, count}, price ->
-      positions =
-        map.grid
-        |> Map.filter(fn {_, position_label} -> position_label === label end)
-        |> Map.keys()
+#   def merge_edges([], merged), do: merged
 
-      clusters = get_all_clusters(positions, %{width: map.width, height: map.height}, [])
+#   def merge_edges([edge | tail], merged) do
+#     IO.inspect(edge)
+#     # TODO: Find all edges connected to the same points.
+#     # Try to merge each, if they are colinear.
+#     # Delete merged edges and add to merged.
+#   end
 
-      prices =
-        clusters
-        |> Enum.map(fn cluster ->
-          [perimeter, border_points] =
-            find_perimeter(
-              [hd(cluster)],
-              cluster,
-              %{width: map.width, height: map.height},
-              MapSet.new(),
-              0,
-              []
-            )
+#   defp part_one(map) do
+#     map.labels
+#     # |> Map.filter(fn {label, _count} -> label === "A" end)
+#     |> Enum.reduce(0, fn {label, count}, price ->
+#       positions =
+#         map.grid
+#         |> Map.filter(fn {_, position_label} -> position_label === label end)
+#         |> Map.keys()
 
-          IO.inspect(create_edges(border_points))
-          # IO.inspect([label, border_points], label: "Border")
-          perimeter * length(cluster)
-        end)
+#       clusters = get_all_clusters(positions, %{width: map.width, height: map.height}, [])
 
-      price + Enum.sum(prices)
-    end)
-  end
+#       prices =
+#         clusters
+#         |> Enum.map(fn cluster ->
+#           [perimeter, border_points] =
+#             find_perimeter(
+#               [hd(cluster)],
+#               cluster,
+#               %{width: map.width, height: map.height},
+#               MapSet.new(),
+#               0,
+#               []
+#             )
 
-  defp part_two(stones), do: :noop
+#           create_edges(border_points)
 
-  def run(_) do
-    data = Timer.measure(fn -> parse_input() end, "Input")
-    p1_result = Timer.measure(fn -> part_one(data) end, "Part 1")
-    # p2_result = Timer.measure(fn -> part_two(data) end, "Part 2")
-    IO.puts("| Part one: #{p1_result} |")
-    # IO.puts("| Part two: #{p2_result} |")
-  end
-end
+#           perimeter * length(cluster)
+#         end)
+
+#       price + Enum.sum(prices)
+#     end)
+#   end
+
+#   defp part_two(stones), do: :noop
+
+#   def run(_) do
+#     data = Timer.measure(fn -> parse_input() end, "Input")
+#     p1_result = Timer.measure(fn -> part_one(data) end, "Part 1")
+#     # p2_result = Timer.measure(fn -> part_two(data) end, "Part 2")
+#     IO.puts("| Part one: #{p1_result} |")
+#     # IO.puts("| Part two: #{p2_result} |")
+#   end
+# end
